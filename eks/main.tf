40e8c5798688fb742a8b910f5669dcdfc98d4017
Alice <xinranjiang2015@yahoo.com>
2024-04-10 15:21:48 -0400

    fix

--- eks/main.tf
+++ eks/main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
 
 data "aws_availability_zones" "azs" {}
 
---
+++ jenkins/terraform.tfvars
@@ -0,0 +1,3 @@
+vpc_cidr       = "172.32.5.0/24"
+public_subnets = ["172.32.5.16/28"]
+instance_type  = "t2.xlarge"
